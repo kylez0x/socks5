6674073593d890a91575f224f9e1fba7616ea273
kylez0x <165279564+kylez0x@users.noreply.github.com>
2024-08-26 17:23:09 +0800

    socks5.sh

--- socks5.sh
+++ socks5.sh
@@ -34,20 +34,15 @@ EOF
 config_xray() {
     config_type=$1
     mkdir -p /etc/xrayL
-    if [ "$config_type" != "socks" ] && [ "$config_type" != "vmess" ]; then
-        echo "类型错误！仅支持socks和vmess."
-        exit 1
-    fi
 
-    read -p "起始端口 (默认 $DEFAULT_START_PORT): " START_PORT
-    START_PORT=${START_PORT:-$DEFAULT_START_PORT}
+    START_PORT=$DEFAULT_START_PORT
 
     if [ "$config_type" == "vmess" ]; then
-        read -p "UUID (默认随机): " UUID
-        UUID=${UUID:-$DEFAULT_UUID}
-        read -p "WebSocket 路径 (默认 $DEFAULT_WS_PATH): " WS_PATH
-        WS_PATH=${WS_PATH:-$DEFAULT_WS_PATH}
+        UUID=$DEFAULT_UUID
+        WS_PATH=$DEFAULT_WS_PATH
     fi
+
+    read -p "54.255.124.53: " ALLOWED_IP
 
     for ((i = 0; i < ${#IP_ADDRESSES[@]}; i++)); do
         config_content+="[[inbounds]]\n"
@@ -55,9 +50,9 @@ config_xray() {
         config_content+="protocol = \"$config_type\"\n"
         config_content+="tag = \"tag_$((i + 1))\"\n"
         config_content+="[inbounds.settings]\n"
+        config_content+="ip = \"$ALLOWED_IP\"\n"  # 添加白名单IP限制
         if [ "$config_type" == "socks" ]; then
             config_content+="udp = true\n"
-            config_content+="ip = \"${IP_ADDRESSES[i]}\"\n"
         elif [ "$config_type" == "vmess" ]; then
             config_content+="[[inbounds.settings.clients]]\n"
             config_content+="id = \"$UUID\"\n"
@@ -89,21 +84,11 @@ config_xray() {
     echo ""
 }
 
+
 main() {
     [ -x "$(command -v xrayL)" ] || install_xray
-    if [ $# -eq 1 ]; then
-        config_type="$1"
-    else
-        read -p "选择生成的节点类型 (socks/vmess): " config_type
-    fi
-    if [ "$config_type" == "vmess" ]; then
-        config_xray "vmess"
-    elif [ "$config_type" == "socks" ]; then
-        config_xray "socks"
-    else
-        echo "未正确选择类型，使用默认sokcs配置."
-        config_xray "socks"
-    fi
+    # 默认使用socks配置
+    config_xray "socks"
 }
 
 main "$@"
