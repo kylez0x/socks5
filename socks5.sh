32215877290f2a0d293e4442e16cbbed611cddd6
kylez0x <165279564+kylez0x@users.noreply.github.com>
2025-02-25 22:18:36 +0800

    Update socks5.sh

--- socks5.sh
+++ socks5.sh
@@ -1,4 +1,4 @@
-DEFAULT_START_PORT=20000                         #默认起始端口
+DEFAULT_START_PORT=20000                         #默认socks5起始端口
 DEFAULT_SOCKS_USERNAME="userb"                   #默认socks账号
 DEFAULT_SOCKS_PASSWORD="passwordb"               #默认socks密码
 DEFAULT_WS_PATH="/ws"                            #默认ws路径
@@ -42,95 +42,71 @@ config_xray() {
         exit 1
     fi
 
-    read -p "起始端口 (默认 $DEFAULT_START_PORT): " START_PORT
+    # SOCKS5 配置部分
+    read -p "SOCKS 起始端口 (默认 $DEFAULT_START_PORT): " START_PORT
     START_PORT=${START_PORT:-$DEFAULT_START_PORT}
-    if [ "$config_type" == "socks" ]; then
-        read -p "SOCKS 账号 (默认 $DEFAULT_SOCKS_USERNAME): " SOCKS_USERNAME
-        SOCKS_USERNAME=${SOCKS_USERNAME:-$DEFAULT_SOCKS_USERNAME}
+    read -p "SOCKS 账号 (默认 $DEFAULT_SOCKS_USERNAME): " SOCKS_USERNAME
+    SOCKS_USERNAME=${SOCKS_USERNAME:-$DEFAULT_SOCKS_USERNAME}
 
-        read -p "SOCKS 密码 (默认 $DEFAULT_SOCKS_PASSWORD): " SOCKS_PASSWORD
-        SOCKS_PASSWORD=${SOCKS_PASSWORD:-$DEFAULT_SOCKS_PASSWORD}
-    elif [ "$config_type" == "vmess" ]; then
-        read -p "UUID (默认随机): " UUID
-        UUID=${UUID:-$DEFAULT_UUID}
-        read -p "WebSocket 路径 (默认 $DEFAULT_WS_PATH): " WS_PATH
-        WS_PATH=${WS_PATH:-$DEFAULT_WS_PATH}
-    elif [ "$config_type" == "http" ]; then
-        read -p "HTTP 起始端口 (默认 $DEFAULT_HTTP_PORT): " HTTP_PORT
-        HTTP_PORT=${HTTP_PORT:-$DEFAULT_HTTP_PORT}
-    fi
+    read -p "SOCKS 密码 (默认 $DEFAULT_SOCKS_PASSWORD): " SOCKS_PASSWORD
+    SOCKS_PASSWORD=${SOCKS_PASSWORD:-$DEFAULT_SOCKS_PASSWORD}
+
+    # HTTP 配置部分
+    read -p "HTTP 起始端口 (默认 $DEFAULT_HTTP_PORT): " HTTP_PORT
+    HTTP_PORT=${HTTP_PORT:-$DEFAULT_HTTP_PORT}
 
     for ((i = 0; i < ${#IP_ADDRESSES[@]}; i++)); do
+        # SOCKS 配置
         config_content+="[[inbounds]]\n"
-        if [ "$config_type" == "http" ]; then
-            config_content+="port = $((HTTP_PORT + i))\n"
-            config_content+="protocol = \"http\"\n"
-        else
-            config_content+="port = $((START_PORT + i))\n"
-            config_content+="protocol = \"$config_type\"\n"
-        fi
-        config_content+="tag = \"tag_$((i + 1))\"\n"
+        config_content+="port = $((START_PORT + i))\n"
+        config_content+="protocol = \"socks\"\n"
+        config_content+="tag = \"tag_socks_$((i + 1))\"\n"
         config_content+="[inbounds.settings]\n"
-        if [ "$config_type" == "socks" ]; then
-            config_content+="auth = \"password\"\n"
-            config_content+="udp = true\n"
-            config_content+="ip = \"${IP_ADDRESSES[i]}\"\n"
-            config_content+="[[inbounds.settings.accounts]]\n"
-            config_content+="user = \"$SOCKS_USERNAME\"\n"
-            config_content+="pass = \"$SOCKS_PASSWORD\"\n"
-        elif [ "$config_type" == "vmess" ]; then
-            config_content+="[[inbounds.settings.clients]]\n"
-            config_content+="id = \"$UUID\"\n"
-            config_content+="[inbounds.streamSettings]\n"
-            config_content+="network = \"ws\"\n"
-            config_content+="[inbounds.streamSettings.wsSettings]\n"
-            config_content+="path = \"$WS_PATH\"\n\n"
-        fi
+        config_content+="auth = \"password\"\n"
+        config_content+="udp = true\n"
+        config_content+="ip = \"${IP_ADDRESSES[i]}\"\n"
+        config_content+="[[inbounds.settings.accounts]]\n"
+        config_content+="user = \"$SOCKS_USERNAME\"\n"
+        config_content+="pass = \"$SOCKS_PASSWORD\"\n"
+
+        # HTTP 配置
+        config_content+="[[inbounds]]\n"
+        config_content+="port = $((HTTP_PORT + i))\n"
+        config_content+="protocol = \"http\"\n"
+        config_content+="tag = \"tag_http_$((i + 1))\"\n"
+        config_content+="[inbounds.settings]\n"
+        config_content+="ip = \"${IP_ADDRESSES[i]}\"\n"
+
+        # 出站配置
         config_content+="[[outbounds]]\n"
         config_content+="sendThrough = \"${IP_ADDRESSES[i]}\"\n"
         config_content+="protocol = \"freedom\"\n"
         config_content+="tag = \"tag_$((i + 1))\"\n\n"
         config_content+="[[routing.rules]]\n"
         config_content+="type = \"field\"\n"
-        config_content+="inboundTag = \"tag_$((i + 1))\"\n"
+        config_content+="inboundTag = \"tag_socks_$((i + 1))\"\n"
+        config_content+="outboundTag = \"tag_$((i + 1))\"\n\n"
+
+        config_content+="[[routing.rules]]\n"
+        config_content+="type = \"field\"\n"
+        config_content+="inboundTag = \"tag_http_$((i + 1))\"\n"
         config_content+="outboundTag = \"tag_$((i + 1))\"\n\n\n"
     done
+
     echo -e "$config_content" >/etc/xrayL/config.toml
     systemctl restart xrayL.service
     systemctl --no-pager status xrayL.service
     echo ""
-    echo "生成 $config_type 配置完成"
-    echo "起始端口:$START_PORT"
+    echo "生成 SOCKS5 和 HTTP 配置完成"
+    echo "SOCKS起始端口:$START_PORT"
+    echo "HTTP起始端口:$HTTP_PORT"
     echo "结束端口:$(($START_PORT + $i - 1))"
-    if [ "$config_type" == "socks" ]; then
-        echo "socks账号:$SOCKS_USERNAME"
-        echo "socks密码:$SOCKS_PASSWORD"
-    elif [ "$config_type" == "vmess" ]; then
-        echo "UUID:$UUID"
-        echo "ws路径:$WS_PATH"
-    elif [ "$config_type" == "http" ]; then
-        echo "HTTP起始端口:$HTTP_PORT"
-    fi
     echo ""
 }
 
 main() {
     [ -x "$(command -v xrayL)" ] || install_xray
-    if [ $# -eq 1 ]; then
-        config_type="$1"
-    else
-        read -p "选择生成的节点类型 (socks/vmess/http): " config_type
-    fi
-    if [ "$config_type" == "vmess" ]; then
-        config_xray "vmess"
-    elif [ "$config_type" == "socks" ]; then
-        config_xray "socks"
-    elif [ "$config_type" == "http" ]; then
-        config_xray "http"
-    else
-        echo "未正确选择类型，使用默认sokcs配置."
-        config_xray "socks"
-    fi
+    config_xray "socks" # 生成socks5和http代理配置
 }
 
 main "$@"
